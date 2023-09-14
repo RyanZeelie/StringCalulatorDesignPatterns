84e25b5205858386b45403169deb7f9c47dbbaf3
Ryan Zeelie <ryan.zeelie@kaleidocode.co.za>
2023-09-14 10:10:06 +0800

    Added test for AdditionNumberService for parsing input that has default delimiters

---
+++ StringCalculator/CalculatorTests/NumberService/AdditionNumberServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Calculator.Enums;
+using Calculator.Factories;
+using Calculator.Services.Delimiters;
+using Calculator.Services.Numbers;
+using NSubstitute;
+
+namespace CalculatorTests.NumberService
+{
+    [TestFixture]
+    public class AdditionNumberServiceTests
+    {
+        private INumberServiceFactory _mockNumberServiceFactory;
+        private INumberService _numberService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockNumberServiceFactory = Substitute.For<INumberServiceFactory>();
+            _mockNumberServiceFactory.CreateNumberService(Operations.Add).Returns(CreateNumberService());
+
+            _numberService = _mockNumberServiceFactory.CreateNumberService(Operations.Add);
+        }
+
+        [TestCase("1,2,3")]
+        [TestCase("1\n2\n3")]
+        public void GIVEN_NumbersSeperatedByDefaultDelimiters_WHEN_ParsingNumbers_RETURNS_ListOfNumbersAsInt(string input)
+        {
+            // Arrange
+            var expectedResult = new List<int>() { 1, 2, 3 };
+
+            // Act
+            var result = _numberService.ParseNumbers(input);
+
+            // Assert
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+
+        private INumberService CreateNumberService()
+        {
+            var delimiterService = new AdditionDelimiterService();
+            var numberService = new AdditionNumberService(delimiterService);
+
+            return numberService;
+        }
+    }
+}
