f54de77c1d19be02d84a459adec2dd5250dbde13
Ryan Zeelie <ryan.zeelie@kaleidocode.co.za>
2023-09-14 10:05:41 +0800

    Added tests for number service factory

---
+++ StringCalculator/CalculatorTests/NumberServiceFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Calculator.Enums;
+using Calculator.Factories;
+using Calculator.Services.Numbers;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class NumberServiceFactoryTests
+    {
+        private INumberServiceFactory _numberServiceFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _numberServiceFactory = new NumberServiceFactory();
+        }
+
+        [Test]
+        public void GIVEN_AddOperation_WHEN_CreatingNumberService_RETURNS_AdditionNumberService()
+        {
+            // Act
+            var AdditionNumberService = _numberServiceFactory.CreateNumberService(Operations.Add);
+
+            //Asset
+            Assert.IsInstanceOf<AdditionNumberService>(AdditionNumberService);
+        }
+
+        [Test]
+        public void GIVEN_SubtractOperation_WHEN_CreatingNumberService_RETURNS_SubtractionNumberService()
+        {
+            // Act
+            var subtractionNumberService = _numberServiceFactory.CreateNumberService(Operations.Subtract);
+
+            //Asset
+            Assert.IsInstanceOf<SubtractionNumberService>(subtractionNumberService);
+        }
+    }
+}
